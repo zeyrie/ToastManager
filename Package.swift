ccc14a3891c9d0a29eed6cad686918529ad3a77f
Abilash <abilash@zeyrie.dev>
2024-06-25 23:35:33 +0530

    Feat add support for macOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "ToastManager",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
